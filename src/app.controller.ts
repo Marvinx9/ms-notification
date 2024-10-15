04fd4848ed03f14de7b0cf53d8fb5d1e6d80ba2d
Afranio Nunes Dantas <afranio.dantas@livsaude.com.br>
2024-10-15 14:38:32 -0300

    feat: create micro-service send-email

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
+import { MailerService } from '@nestjs-modules/mailer';
+
+type NotificationDto = {
+  email: string;
+  startAt: Date;
+  endAt: Date;
+  name: string;
+  title: string;
+  description: string;
+};
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private readonly mailerService: MailerService) {}
 
   @EventPattern('task_notification')
-  taskNotification(data: any) {
-    console.log('Recebendo mensagem');
-    console.log(data);
+  async taskNotification(data: NotificationDto) {
+    await this.mailerService.sendMail({
+      to: data.email,
+      from: 'taskmanager@gmail.com.br',
+      subject: 'Notificação de tarefa',
+      html: `
+        <body>
+          <h1>Olá ${data.name} </h1>
+
+          <span>Você tem uma tarefa para hoje</span>
+          <br/>
+          <span>Título: ${data.title}</span>
+          <br/>
+          <span>Descrição: ${data.description}</span>
+          <br/>
+          <span>Início: ${data.startAt}</span>
+          <br/>
+          <span>Fim: ${data.endAt}</span>
+        </body>
+      `,
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: parseInt(process.env.MAIL_PORT),
+        secure: false,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
